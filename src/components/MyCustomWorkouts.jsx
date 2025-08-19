d4a02b68ba61f80d0eae3143802406911bfc450d
ludobruel9131-max <ludo.bruel9131@gmail.com>
2025-08-19 16:17:45 +0200

    Update MyCustomWorkouts.jsx

--- src/components/MyCustomWorkouts.jsx
+++ src/components/MyCustomWorkouts.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
+import { Button } from "./components/ui/button";
 import { Dumbbell, Play, Trash2 } from "lucide-react";
 import { useApp } from "../App";
 import { useNavigate } from "react-router-dom";
