f206036a04de2b1bcf54782a01046886f71ff479
ludobruel9131-max <ludo.bruel9131@gmail.com>
2025-08-19 16:55:53 +0200

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,7 +3,7 @@ import { Flame, BarChart2, Crown, Timer as TimerIcon, CheckCircle2 } from "lucid
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { ResponsiveContainer, AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip, BarChart, Bar } from "recharts";
 import { useApp } from "../App";
-import { bmi, formatDate } from "../lib/utils";
+import { bmi, formatDate } from "../../lib/utils";
 import { Button } from "./ui/button";
 import { useNavigate } from "react-router-dom";
 
