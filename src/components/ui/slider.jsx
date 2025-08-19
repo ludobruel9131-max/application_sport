6dcf22924028b3db4a7b7be419f6245dd0f643af
ludobruel9131-max <ludo.bruel9131@gmail.com>
2025-08-19 16:48:26 +0200

    Update slider.jsx

--- src/components/ui/slider.jsx
+++ src/components/ui/slider.jsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import * as SliderPrimitive from "@radix-ui/react-slider"
-import { cn } from "@/lib/utils"
+import { cn } from "src/components/ui/lib/utils"
 
 const Slider = React.forwardRef(({ className, ...props }, ref) => (
   <SliderPrimitive.Root
