c9acf229c8a13d32f59a83d7f912e8524b5af78b
ludobruel9131-max <ludo.bruel9131@gmail.com>
2025-08-20 08:42:27 +0200

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,14 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// Assurez-vous que les variables exerciseMET, weight et duration sont définies
+// d'où cette fonction est appelée.
+export function caloriesForExercise(duration, weight, exerciseMET) {
+  // Cette formule est une estimation basée sur les MET (Metabolic Equivalent of Task).
+  const calories = 0.0175 * exerciseMET * weight * duration;
+  return calories;
 }
