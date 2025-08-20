e1c6c90c2b981d10e3ed9f5be99d6ad469d261aa
ludobruel9131-max <ludo.bruel9131@gmail.com>
2025-08-20 12:16:22 +0200

    Update label.jsx

--- src/components/ui/label.jsx
+++ src/components/ui/label.jsx
@@ -1,24 +1,19 @@
-// src/components/ui/label.jsx
-
 import * as React from "react"
 import { cva } from "class-variance-authority";
+import { cn } from "../../lib/utils";
 
-// Définition des variantes pour le style du label
 const labelVariants = cva(
   "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
 );
 
-// Utilisation de React.forwardRef pour permettre le passage de 'ref' aux composants
 const Label = React.forwardRef(({ className, ...props }, ref) => (
-  // Le composant rend un simple élément <label> avec les styles appliqués
   <label
     ref={ref}
-    className={labelVariants({ className })}
+    className={cn(labelVariants(), className)}
     {...props}
   />
 ));
 
-// Définition d'un nom d'affichage pour le composant
 Label.displayName = "Label";
 
 export { Label, labelVariants };
